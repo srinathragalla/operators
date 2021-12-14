c27efde6492c4a53a47981326f13099a3ae0b7e7
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 10:15:14 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,38 @@
+var a=50;
+var b=150;
+var sum=a+b;
+console.log(sum);
+
+var a=9;
+var b=10;
+var diff=a-b;
+console.log(diff)
+
+var a=5;
+var b=5;
+var mul=a*b;
+console.log(mul);
+
+var a=10;
+var b=5;
+var div=a/b;
+console.log(div);
+
+var a=15;
+var b=4;
+var modulus=a%b;
+console.log(modulus);
+
+var a=5;
+a++;                     
+var b=a;
+console.log(b);
+
+var a=10;
+a--;
+var b=a;
+console.log(b);
+
+
+
+
